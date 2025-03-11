dc9660d48f81fd628b231416f5f5524f665cc783
one-cloud-dev <cloud01rer@gmail.com>
2025-03-11 15:06:44 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,25 @@
 function playSound(soundId) {
-    var sound = document.getElementById(soundId);
-    sound.play();
+  var sound = document.getElementById(soundId);
+  
+  if (sound) {  // Проверяем, существует ли элемент
+    if (sound.paused) {
+      sound.play();  // Если звук был на паузе, воспроизводим его
+    } else {
+      sound.pause();  // Если звук уже играет, ставим его на паузу
+      sound.currentTime = 0;  // Останавливаем его и возвращаем в начало
+      sound.play();  // Снова начинаем воспроизведение
+    }
+  } else {
+    console.error('Звук с таким ID не найден:', soundId);  // Сообщение об ошибке, если звука нет
   }
+}
+
+window.onload = function() {
+  // Проверяем параметр в URL, например: mysite.com/?sound=sound1
+  const urlParams = new URLSearchParams(window.location.search);
+  const sound = urlParams.get('sound');
+  
+  if (sound) {
+    playSound(sound);  // Если параметр sound есть, вызываем функцию воспроизведения
+  }
+};
